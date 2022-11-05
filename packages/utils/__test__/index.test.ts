64b9b1e9bbec76530c45bfb3cc4d186206969abb
高原 <gaoyuan200408@credithc.com>
2022-11-05 23:39:02 +0800

    feat:增加工具库以及测试文件

---
+++ packages/utils/__test__/index.test.ts
@@ -0,0 +1,17 @@
+import { describe, it, expect } from 'vitest'
+import { isObject, isOn } from '../src'
+
+describe('测试工具库', () => {
+  it('测试isObject', () => {
+    expect(1 + 2).toBe(3)
+    expect(isObject({})).toBe(true)
+    expect(isObject(1)).toBe(false)
+    expect(isObject(null)).toBe(false)
+  })
+
+  it('测试isOn函数', () => {
+    expect(isOn('onClick')).toBe(true)
+    //  这里 ts 类型会报错
+    // expect(isOn(1)).toBe(false)
+  })
+})
