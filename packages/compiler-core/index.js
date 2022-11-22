0d87a1ef5b3ba13222855295b1dc72ae256403ab
MineMine <gaoyuan_flying@163.com>
2022-11-23 00:18:30 +0800

    feat:静态标记

--- packages/compiler-core/index.js
+++ packages/compiler-core/index.js
@@ -1,6 +1,6 @@
 const template = `
   <div id="app">
-    <div @click="{console.log('hi')}" :id="name">{{name}}</div>
+    <div @click="{console.log('hi')}" :id="name">你好，{{name}}</div>
     <h1 :name="title">Vue编译</h1>
     <p class="container">编译学习</p>
   </div>
@@ -106,8 +106,73 @@ function parse (template) {
   return ast
 }
 
-//  标记一些 vue 的语法，
-function transform () { }
+const PatchFlags = {
+  TEXT: 1,
+  CLASS: 1 << 1,
+  STYLE: 1 << 2,
+  PROPS: 1 << 3,
+  EVENT: 1 << 4,
+}
+
+function transform (ast) {
+  // 需要收集桑当前模板依赖的函数，这样 generate 才能生成 import 语句
+  const context = {
+    helpers: new Set(['openBlock', 'createElementVNode', 'createElementBlock']),
+  }
+  // 标记一些 vue 的语法，vue3中多了一些静态标记
+  // 遍历 ast 这棵树，对 vue 语法进行转换，对节点是否静态进行标记
+  travest(ast, context)
+}
+
+function travest (ast, context) {
+  // 需要嵌套
+  const re = /\{\{(.*)\}\}/g
+  switch (ast.type) {
+    case 'root':
+    case 'element':
+      context.helpers.add('openBlock')
+      context.helpers.add('createElementBlock')
+      ast.children.forEach((astNode) => {
+        travest(astNode, context)
+      })
+      ast.props = ast.props.map((prop) => {
+        const { key, val } = prop
+        if (key[0] === '@') {
+          return {
+            key: `on${key[1].toUpperCase()}${key.slice(2)}`,
+            val,
+          }
+        } else if (key[0] === ':') {
+          // 属性的操作函数不同 diff 的时候做不同的标记
+          // class
+          // styles
+          // 别的属性
+          return {
+            key: key.slice(1),
+            val,
+          }
+        } else if (key.startsWith('v-')) {
+          // v-model v-if
+          return prop
+        }
+        // 所有的 vue 语法都没识别，这里就是静态的
+        return { ...prop, static: true }
+      })
+      break
+    case 'text':
+      if (re.test(ast.val)) {
+        // 说明有 变量
+        context.helpers.add('toDisplayString')
+        ast.val = ast.val.replace(re, (s0, s1) => {
+          return s1 // 直接替换，默认变量
+        })
+      } else {
+        // 普通的文本
+      }
+      break
+  }
+  return ast
+}
 
 // 标记之后的 ast 生成render 函数
 function generate () { }
@@ -116,8 +181,8 @@ function compile (template) {
   // 模板解析成 render 函数
   // 第一步：生成 Ast
   const ast = parse(template.trim())
+  transform(ast)
   console.log(JSON.stringify(ast, null, 2))
-  // transform(ast)
 }
 
 compile(template)
